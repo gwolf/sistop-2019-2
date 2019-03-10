7c4907cd9219ecbfb645ceed3a9ed94afca25ad5
ValdrST <valdr.stiglitz@gmail.com>
2019-03-09 23:28:33 -0600

    creada la estructura inicial del proyecto

---
+++ proyectos/2/RomeroVicente/VpostHorde/core/console.py
@@ -0,0 +1,8 @@
+import argparse
+
+class Console:
+    def __init__(self):
+        self.parser =  argparse.ArgumentParser()
+    
+    def evaluar_argumentos(self):
+        print("evaluando argumentos")
---
+++ proyectos/2/RomeroVicente/VpostHorde/main.py
@@ -0,0 +1,5 @@
+from core.console import Console
+
+if __name__ == "__main__":
+    console = Console()
+    console.evaluar_argumentos()
