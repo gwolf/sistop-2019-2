8a4ebb6042a90e1e5aa407e1888953dddea2a407
gonzalez_fgz@hotmail.com <gonzalez_fgz@hotmail.com>
2019-03-13 21:20:38 -0600

    Terminamos la ejecucion con un "return 0"

--- practicas/3/GonzalezFelix/hilos.c
+++ practicas/3/GonzalezFelix/hilos.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-
-void *procesoSeparado(void *data)
+#include <windows.h>
+ 
+DWORD WINAPI hilo(LPVOID LPARAM);
+ 
+ 
+int main()
 {
-	char *texto = (char *) data;
-	struct timespec tiempo = {1, 0};
-	while (1) {
-		printf("%s\n",texto);
-		pthread_delay_np(&tiempo);
-	}
-	}
-
-
-int main(void)
+HANDLE MiHilo;
+DWORD  IdDelHilo;
+ 
+MiHilo=CreateThread(NULL,0,hilo,NULL,0,& IdDelHilo);
+ MessageBox(NULL,"2","ventana",MB_OK);
+ 
+return 0;
+}
+ 
+DWORD WINAPI hilo(LPVOID LPARAM)
 {
-	pthread_t proceso1;
-	pthread_t proceso2;
-	pthread_create(&proceso1, NULL, &procesoSeparado, "hola");
-	pthread_create(&proceso2, NULL, &procesoSeparado, "adios");
-    pthread_join(proceso1, NULL);
-    pthread_join(proceso2, NULL);
-    return 0;
+   MessageBox(NULL,"1","ventana",MB_OK);
+ 
 }
