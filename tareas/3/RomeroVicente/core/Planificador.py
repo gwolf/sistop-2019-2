83dd39d5fa580cacded4562df6d81f074843ab03
ValdrST <valdr.stiglitz@gmail.com>
2019-03-18 23:12:54 -0600

    Añadido el calculador de promedios

--- tareas/3/RomeroVicente/core/Planificador.py
+++ tareas/3/RomeroVicente/core/Planificador.py
@@ -4,6 +4,10 @@ class Planificador:
         self.quantum = quantum
         self.procesos = procesos
         self.total = 0
+        self.T_list = []
+        self.P_list = []
+        self.R_list = []
+        self.E_list = []
     
     def mostrar_procesos(self):
         texto = ""
@@ -13,3 +17,22 @@ class Planificador:
             texto = texto + " " + proceso.nombre +": " + str(proceso.llegada) + ", t="+ str(proceso.t)+";"
         self.total = suma
         print(texto + "(total:"+str(self.total)+")")
+
+    def get_promedios(self):
+        P_promedio = 0
+        R_promedio = 0
+        T_promedio = 0
+        E_promedio = 0
+        cantidad_procesos = len(self.procesos)
+        for P in self.P_list:
+            P_promedio = P_promedio + P
+        P_promedio = P_promedio/cantidad_procesos
+        for T in self.T_list:
+            T_promedio = T_promedio + T
+        T_promedio = T_promedio/cantidad_procesos
+        for R in self.R_list:
+            R_promedio = R_promedio + R
+        for E in self.E_list:
+            E_promedio = E_promedio + E
+        R_promedio = R_promedio/cantidad_procesos
+        return {"P":P_promedio,"R":R_promedio,"T":T_promedio,"E":E_promedio}
