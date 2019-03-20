426bd895a16b647dd153a36f8bc9e098d5250834
ValdrST <valdr.stiglitz@gmail.com>
2019-03-20 00:01:38 -0600

    Cambiado el rango de generador de proceso

--- tareas/3/RomeroVicente/core/Proceso.py
+++ tareas/3/RomeroVicente/core/Proceso.py
@@ -2,6 +2,6 @@ from random import randint
 class Proceso:
     def __init__(self):
         self.nombre = "["+str(randint(10,99))+"]"
-        self.t = randint(1,10) # Asigna un tiempo de ejecucion t entre 1 y 10 quantum
+        self.t = randint(320,800) # Asigna un tiempo de ejecucion t entre 160 y 800 ticks
         self.llegada = randint(1,20)
     
