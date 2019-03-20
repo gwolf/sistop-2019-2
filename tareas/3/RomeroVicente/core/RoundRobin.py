c5e3f6c524d4a1e22a994ba40fed50ccbea324a0
ValdrST <valdr.stiglitz@gmail.com>
2019-03-20 00:00:23 -0600

    Terminado el round robin

--- tareas/3/RomeroVicente/core/RoundRobin.py
+++ tareas/3/RomeroVicente/core/RoundRobin.py
@@ -1,6 +1,7 @@
 from random import randint
 from core.Planificador import Planificador
-class Round_robin(Planificador):
+from math import ceil
+class RoundRobin(Planificador):
     def __init__(self,log,quantum,procesos):
         Planificador.__init__(self,log,quantum,procesos)
     
@@ -9,4 +10,57 @@ class Round_robin(Planificador):
         self.planificar()
     
     def planificar(self):
-        pass
+        texto = ""
+        total = 0
+        procesos_listos = []
+        for proceso in self.procesos:
+            proceso = {"nombre":proceso.nombre,"t":ceil(proceso.t/self.quantum),"quantum":ceil(proceso.t/self.quantum),"llegada":proceso.llegada,"inicio":-1,"fin":0}
+            procesos_listos.append(proceso)
+        procesos_terminados = []
+        texto = ""
+        while(len(procesos_listos) > 0):
+            procesos_temp = []
+            avant = False
+            for proceso in procesos_listos:
+                if(proceso["quantum"] > 0):
+                    if(proceso["llegada"] > total and total == 0):
+                        proceso["inicio"] = proceso["llegada"]
+                        total = proceso["inicio"]
+                        texto = texto + proceso["nombre"]
+                        proceso["quantum"] = (proceso["quantum"] - 1)
+                        total = total + 1
+                        avant = True
+                    elif(proceso["llegada"] == total and proceso["inicio"]== -1):
+                        proceso["inicio"] = total
+                        texto = texto + proceso["nombre"]
+                        proceso["quantum"] = proceso["quantum"] - 1
+                        total = total + 1
+                        avant = True
+                    elif(proceso["llegada"] < total):
+                        if(proceso["inicio"] < 0):
+                            proceso["inicio"] = total
+                        texto = texto + proceso["nombre"]
+                        proceso["quantum"] = proceso["quantum"] -1
+                        total = total + 1
+                        avant = True
+                    procesos_temp.append(proceso)
+                else:
+                    proceso["fin"] = total
+                    procesos_terminados.append(proceso)
+            if(avant == False):
+                texto = texto + "[  ]"
+                total = total + 1
+            procesos_listos = procesos_temp
+        for proceso in procesos_terminados:
+            T = proceso["fin"] - proceso["llegada"]
+            self.T_list.append(T)
+            P = T/proceso["t"]
+            self.P_list.append(P)
+            R = proceso["t"]/T
+            self.R_list.append(R)
+            E = T - proceso["t"]
+            self.E_list.append(E)
+        
+        promedios = self.get_promedios()
+        print("RR1: T={0}, E={1}, P={2}".format(promedios['T'],promedios['E'],promedios['P']))
+        print(texto)
