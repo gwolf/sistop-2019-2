3dd1709f54085cbb607fbafc802552e75fb39286
ValdrST <valdr.stiglitz@gmail.com>
2019-03-20 00:00:56 -0600

    Agregada variable y calculo correcto

--- tareas/3/RomeroVicente/core/FCFS.py
+++ tareas/3/RomeroVicente/core/FCFS.py
@@ -1,18 +1,15 @@
 from random import randint
 from core.Planificador import Planificador
+from math import ceil
 class FCFS(Planificador):
     def __init__(self,log,quantum,procesos):
         Planificador.__init__(self,log,quantum,procesos)
  
-    
     def iniciar_planificador(self):
         self.mostrar_procesos()
         self.planificar()
 
-    
-    
     def planificar(self):
-        self.procesos.sort(key=lambda x:x.llegada,reverse=False) #ordena por el orden de llegada
         texto = ""
         total = 0
         for proceso in self.procesos:
@@ -21,20 +18,19 @@ class FCFS(Planificador):
                 total = total + inicio
             else:
                 inicio = total
-            fin = inicio + proceso.t
-            T = fin - proceso.llegada
+            fin = inicio + ceil(proceso.t/self.quantum)
+            T = (fin - proceso.llegada)
+            quantum_tick = proceso.t/self.quantum
             self.T_list.append(T)
-            P = T/proceso.t
+            P = T/quantum_tick
             self.P_list.append(P)
-            R = proceso.t/T
+            R = quantum_tick/T
             self.R_list.append(R)
-            E = T - proceso.t
+            E = T - quantum_tick
             self.E_list.append(E)
-            for i in range(proceso.t):
+            for i in range(int(quantum_tick)):
                 texto = texto + proceso.nombre
-            total = total + proceso.t
+            total = total + ceil(proceso.t/self.quantum)
         promedios = self.get_promedios()
         print("FCFS: T={0}, E={1}, P={2}".format(promedios['T'],promedios['E'],promedios['P']))
         print(texto)
-    
-
