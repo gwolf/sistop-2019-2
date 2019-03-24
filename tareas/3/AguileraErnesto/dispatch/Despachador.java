abc1fee0fbb64d06389517e5bcdb04e9f849f830
LeapVader <leapvader_1998@hotmail.com>
2019-03-23 20:45:33 -0600

    Documentation Despachador
    Agrego documentación para el despachador.

--- tareas/3/AguileraErnesto/dispatch/Despachador.java
+++ tareas/3/AguileraErnesto/dispatch/Despachador.java
@@ -1,6 +1,9 @@
 package dispatch;
-
+/* Interfaz que nos proveé el comportamiento estándar de
+*  los algoritmos para despachar procesos, si se necesitan agregar
+*  nuevos algoritmos se debe formar un nuevo contrato para evitar 
+*  romper el código del sistema principal.
+*/
 public interface Despachador{
     public void atiende();
-    //public void cargaProcesos();
 }
