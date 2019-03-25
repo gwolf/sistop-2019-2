89aade54a344dff6daf741ebc00a451ef5e5256b
ValdrST <valdr.stiglitz@gmail.com>
2019-03-24 23:24:39 -0600

    Añadido el main principal

--- proyectos/2/RomeroVicente/VpostHorde/main.py
+++ proyectos/2/RomeroVicente/VpostHorde/main.py
@@ -1,5 +1,15 @@
-from core.console import Console
+import tools.consola as consola
+from tools.GUI import *
+import pkg_resources
+import sys
 
 if __name__ == "__main__":
-    console = Console()
-    console.evaluar_argumentos()
+    e = consola.evaluarArgumentos(params=[])
+    if "--cli" in sys.argv:
+        e.iniciarHilos()
+        analisis = e.crearAnalisis()
+        print(str(analisis.exitosVSFallos)+" "+str(analisis.tiempo_promedio)+" "+str(analisis.state_codes_dict))
+        analisis.dibujar_state_codes()
+    else:
+        app = MyApp(0)
+        app.MainLoop()
