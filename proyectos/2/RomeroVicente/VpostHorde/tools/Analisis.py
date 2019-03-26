f1130dfeba8b074273c0deddd8c5bac347edf28b
vromero <dark_reggae_93@comunidad.unam.mx>
2019-03-26 10:07:36 -0600

    Se añade la implementacion de la grafica en GUI

--- proyectos/2/RomeroVicente/VpostHorde/tools/Analisis.py
+++ proyectos/2/RomeroVicente/VpostHorde/tools/Analisis.py
@@ -1,10 +1,16 @@
 import sys
 import ast
+import wx
+from wx.lib.plot import PlotCanvas, PlotGraphics, PolyLine, PolyMarker
+import numpy as np
+import math
+
 class Analisis(object):
     def __init__(self):
         self.times = []
         self.state_codes = []
         self.estados = []
+        self.fechas = []
         self.tiempo_promedio = 0
         self.exitosVSFallos = {"exitos":0,"fallos":0}
         self.state_codes_dict = {}
@@ -14,7 +20,7 @@ class Analisis(object):
         for segundos in self.times:
             self.tiempo_promedio = self.tiempo_promedio + segundos
         self.tiempo_promedio = self.tiempo_promedio / len(self.times)
-    
+
     def analizar_estados(self):
         self.exitosVSFallos["exitos"] = 0
         self.exitosVSFallos["fallos"] = 0
@@ -23,6 +29,23 @@ class Analisis(object):
                 self.exitosVSFallos["exitos"] = self.exitosVSFallos["exitos"] + 1
             else:
                 self.exitosVSFallos["fallos"] = self.exitosVSFallos["fallos"] + 1
+
+    def dibujar_state_codes(self):
+        size = int(math.sqrt(float(len(self.state_codes))))
+        data = np.zeros((len(self.state_codes),2))
+        codigos=[]
+        for codigo in self.state_codes:
+            if type(codigo) != int:
+                codigos.append(0)
+            else:
+                codigos.append(codigo)
+        data[:,0] = np.array(range(len(codigos)))
+        data[:,1] = np.array(codigos)
+        linea = PolyLine(data, legend="codigos de estado",colour='red')
+        return PlotGraphics([linea],"Resultados", "Tiempo", "codigo")
+        #plt.matshow(imagen)
+        #plt.show()
+        #plt.close()
 
     def analizar_state_codes(self):
         codigos = []
