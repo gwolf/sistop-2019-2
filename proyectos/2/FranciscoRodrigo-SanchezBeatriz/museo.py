4bf318da3102b77150fe1ffc25104f37509c18cb
RodrigoFrancisco <rodrigofranciscopablo@gmail.com>
2019-03-27 00:54:54 -0600

    agrego archivo main

--- proyectos/2/FranciscoRodrigo-SanchezBeatriz/museo.py
+++ proyectos/2/FranciscoRodrigo-SanchezBeatriz/museo.py
@@ -1,58 +1,10 @@
-from threading import Semaphore,Thread
-from time import sleep
-import random
+from d_museum.abrir import abrir_museo
 
-turista_esp=0
-turista_ing=0
-lista_turista_esp=Semaphore(0)
-lista_turista_ing=Semaphore(0)
-total=4
-mutex_guia=Semaphore(1)
+def main():
+	# Al final tenemos que hacer que se pueda especificar
+	# desde linea de comandos la cantidad de turistas y posi-
+	# blemente de guias a simular.
+    abrir_museo(turistas=20)
 
-
-
-def esp(num):
-
-	global turista_esp,turista_ing,esCap
-	#print("esp %d esperando ..." %num)
-	mutex_guia.acquire()
-	turista_esp+=1
-	print("Turista que habla español %d formado para recorrido" %num)
-	if (turista_ing + turista_esp) >=4:
-		for i in range(turista_ing):
-			lista_turista_ing.release()
-		for i in range(turista_esp):
-			lista_turista_esp.release()
-		turista_ing=0
-		turista_esp=0
-		print("*** Inicia recorrido")
-	
-	mutex_guia.release()
-	
-	lista_turista_esp.acquire()
-	
-
-def ing(num):
-
-	global turista_esp,turista_ing, esCap
-	#print("ing %d esperando ..." %num)
-	mutex_guia.acquire()
-	print("Turista que habla inglés %d listo para recorrido" %num)
-	turista_esp+=1
-	if (turista_ing + turista_esp) >=4:
-		for i in range(turista_ing):
-			lista_turista_ing.release()
-		for i in range(turista_esp):
-			lista_turista_esp.release()
-		turista_ing=0
-		turista_esp=0
-		print("*** Inicia recorrido")
-	mutex_guia.release()
-	
-	lista_turista_ing.acquire()
-
-for i in range(20):
-	Thread(target=esp, args=[i]).start()
-
-for i in range(60):
-	Thread(target=ing, args=[i]).start()
+if __name__ == '__main__':
+    main()
