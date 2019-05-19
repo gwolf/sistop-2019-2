8361c8cb9358238350b087851e2ec49c809353c8
rhofp <rhodfra@gmail.com>
2019-05-18 21:57:11 -0500

    Funcion cpin casi terminada, solo falta actualizar los metadatos

--- proyectos/3/FranciscoRodrigo-SanchezBeatriz/fifs.py
+++ proyectos/3/FranciscoRodrigo-SanchezBeatriz/fifs.py
@@ -1,4 +1,6 @@
 import mmap
+import os # para conocer los metadatos de archivos a ingresar con la fx cpin()
+import math
 
 class SuperBlock :
     """
@@ -72,10 +74,11 @@ class FIFS:
 
     dentry_notused ='AQUI_NO_VA_NADA'
 
-    def ls(self):
+    def inodes(self):
         # usamos del 1-4 clusters, es decir 1024*4 = 4096
         # las entradas miden 64 por lo tanto 4096/64 = 64, entonces el rango
         # del for 0-64
+        inodes = []
         for j in range(0,64):
             # Como analogia a lo que vimos en la seccion de admin de procesos
             # ptrb es un puntero base que nos indica donde empiezan
@@ -85,10 +88,13 @@ class FIFS:
             # El directorio se encuentra en los cluster de 1-4 y cada cluster
             # mide 1024 por lo tanto debemo ir en 1024, el cluser 0 es el
             # superblock
+
             prtb = self.sb.size_cluster + j*self.sb.size_dentry
             i = Inode(self.fs_map[prtb:prtb + self.sb.size_dentry])
             if self.dentry_notused != i.fname:
-                print("%s\t%d\t%d" %(i.fname,i.finit_cluster,i.fsize))
+                #print("%s\t%d\t%d" %(i.fname,i.finit_cluster,i.fsize))
+                inodes.append(i)
+        return inodes
 
     def search(self,fe):
         for j in range(0,64):
@@ -98,6 +104,19 @@ class FIFS:
                 i.numdir = j
                 return i
         return None
+
+    def registerFile(self,fe):
+        for j in rang(0,64):
+            prtb = self.sb.size_cluster + j*self.sb.size_dentry
+            i = Inode(self.fs_map[prtb:prtb + self.sb.size_dentry])
+            if self.dentry_notused == i.fname:
+                # tener cuidado con longitud de nombres
+                self.fs_map[prtb:prtb + i.offset_fname] = # fe pero ya rellenada
+
+    def ls(self):
+        # nodes.sort(reverse=True,key=lambda x: x.finit_cluster)= ordenar por ...
+        for i in self.inodes():
+            print("%s\t%d\t%d" %(i.fname,i.finit_cluster,i.fsize))
 
     def rm(self,fe):
         #Primero buscar si el archivo existe,
@@ -127,12 +146,44 @@ class FIFS:
         print("Not build yet")
         # Pseudocodigo
         # Buscar si no hay un archivo con el nombre recibido
+        # Si sí, pedir que renombre el archivo
         # Si no entonces
         #   buscar un lugar donde quepa el archivo
         #   sino hay lugar, desfragmentamos
         #   si despues de desfragmentar no hay lugar
         #       mandar error
-        # Si sí, pedir que renombre el archivo
+
+        # cargando todos los i-nodes que no tenga la cadena 'AQUI NO VA'
+        # mediante la funcion inodes()
+        inodes = self.inodes()
+        inodes.sort(key=lambda x: x.finit_cluster)
+
+        #PRIMERO VALIDAR SI EL ARCHIVO ES VALIDO
+        # validar si es nombre del archivo no es muy largo
+        # operacion: tamaño de archivo / 1024 y redondear siempre hacia arriba
+        fe_size = os.stat(fe).st_size
+        fe_numclusters = math.ceil(fe_size/self.sb.size_cluster)
+        for j in range(0,len(inodes)-1):
+            i_lastcluster = inodes[j].finit_cluster + math.ceil(inodes[j].fsize/self.sb.size_cluster)
+            #print(i_lastcluster)
+            # espacio en clusters entre archivos
+            cluster_space = inodes[j+1].finit_cluster - i_lastcluster
+            # Usando el algoritmo FIFO, guardamos nuestro archivo en el
+            # primer bloque en el que quepa
+            if fe_numclusters <= cluster_space :
+                #print(cluster_space)
+                f = open(fe,"rb")
+                # vamos a escribir en i_lastcluster + 1 nuestra info
+                fe_prtb = self.sb.size_cluster*(i_lastcluster + 1)
+                fe_prtt = fe_prtb + fe_size
+                #print(prtb)
+                #print(prtt)
+                self.fs_map[fe_prtb:fe_prtt] = f.read()
+                # hacer registro de metadatos
+                registerFile(fe)
+                break
+            else :
+                print("cpin: " + fe + ": file too large")
 
     def close(self):
         self.fs_map.close()
