ec40f67161ec33b16879ed34ea5f6e6076bb4fb3
ValdrST <valdr.stiglitz@gmail.com>
2019-05-20 00:34:05 -0500

    Añadido mensaje de debug

--- proyectos/3/RomeroVicente/core/Console.py
+++ proyectos/3/RomeroVicente/core/Console.py
@@ -1,11 +1,15 @@
 import argparse
+from fuse import FUSE
+from core.FiUnamFS import FiUnamFS
+import logging
 
 class Console:
     def __init__(self):
         self.parser = parser = argparse.ArgumentParser()
-        self.parser.add_argument('-M','--mount-point',nargs='?',type=str, required=True, help='Define el punto de montaje')
-        self.parser.add_argument('-R','--root',nargs='?', type=str, required=True, help='Define la raiz del sistema de archivos')
+        self.parser.add_argument('-M','--mount-point',nargs='?',type=str, required=True, help='Define el directorio que va a ser montado')
+        self.parser.add_argument('-R','--root',nargs='?', type=str, required=True, help='Define la raiz del sistema de archivos o el directorio donde sera montado')
         self.args = parser.parse_args()
 
     def iniciar_consola(self):
-        print(self.args)
+        logging.basicConfig(level=logging.DEBUG)
+        FUSE(FiUnamFS(self.args.root),self.args.mount_point, nothreads=True, foreground=True)
