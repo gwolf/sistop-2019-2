e98c3063dbab8406aa031c7ba05572582954c11a
ValdrST <valdr.stiglitz@gmail.com>
2019-05-20 23:35:58 -0500

    Cambiado a uso con menu interactivo

--- proyectos/3/RomeroVicente/core/Console.py
+++ proyectos/3/RomeroVicente/core/Console.py
@@ -1,15 +1,19 @@
 import argparse
 from fuse import FUSE
 from core.FiUnamFS import FiUnamFS
-import logging
 
 class Console:
     def __init__(self):
         self.parser = parser = argparse.ArgumentParser()
-        self.parser.add_argument('-M','--mount-point',nargs='?',type=str, required=True, help='Define el directorio que va a ser montado')
+        #self.parser.add_argument('-M','--mount-point',nargs='?',type=str, required=True, help='Define el archivo binario que contiene el sistema de archivos')
         self.parser.add_argument('-R','--root',nargs='?', type=str, required=True, help='Define la raiz del sistema de archivos o el directorio donde sera montado')
+        self.parser.add_argument('-N','--name-vol',nargs='?', type=str, help='Define la etiqueta del volumen')
+        self.parser.add_argument('-C','--create', default=False,action="store_true", help='Define si se creara nuevo sistema de archivos')
         self.args = parser.parse_args()
 
     def iniciar_consola(self):
-        logging.basicConfig(level=logging.DEBUG)
-        FUSE(FiUnamFS(self.args.root),self.args.mount_point, nothreads=True, foreground=True)
+        if self.args.create:
+            FiUnamFS.create_new_fs(self.args.root,self.args.name_vol)
+        FS = FiUnamFS(self.args.root)
+        FS.iniciar_menu()
+        #FUSE(FiUnamFS(self.args.root),self.args.mount_point, nothreads=True, foreground=True)
