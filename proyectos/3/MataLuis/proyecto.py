12a4e6aa14bee9be34338200a4d12bf0b035caba
Luis <vluism8@gmail.com>
2019-05-24 00:53:21 -0500

    Agrego funcion de info

--- proyectos/3/MataLuis/proyecto.py
+++ proyectos/3/MataLuis/proyecto.py
@@ -1,10 +1,12 @@
 directorio = 'fiunamfs.img'
 tamano_dato = 64
+aux1 = [0, 10, 20, 40, 47, 52]
+aux2 = [8, 3, 15, 5, 2, 8]
 info = []
 tamano = []
 ubicacion = []
-def obtenerarchivos():
 
+def obtenerarchivos():
     sistema = open(directorio,'r')
     posicion = 1024
     while posicion < 5120:
@@ -18,5 +20,15 @@ def obtenerarchivos():
             sistema.seek(posicion+25)
             ubicacion.append(int(sistema.read(5)))
         posicion = posicion + 64
+
+    sistema.close()
+
+
+def obtenerinfo():
+    sistema = open(directorio,'r')
+    for i in range(5):
+        sistema.seek(aux1(i))
+        query = sistema.read(aux2(i))
+        info.append(query)
 
     sistema.close()
